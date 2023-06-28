60a336c583e3df96c42de64a9edbe1d9084c2f29
Arsenii55 <arseka2512@gmail.com>
2023-06-28 18:27:46 +0300

    Second commit

---
+++ src/ClassCastExeption.java
@@ -0,0 +1,14 @@
+public class ClassCastExeption {
+    public static void main(String[] args) {
+        dz();
+    }
+    int a;
+    private static void dz(){
+        try {
+            ClassCastExeption error = (ClassCastExeption) a;
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
